1e3f29f1af0444fcb12265e71673fab24c84b2ca
dinh ngọc <eonho6582@gmail.com>
2021-11-12 23:06:10 +0700

    fix

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -26,7 +26,7 @@ const MealItemForm = (props) => {
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        ref={amoutInputRef}
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
